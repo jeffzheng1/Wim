880d76042892d20084d7dce48c5540de9d7a5dd5
Jeff Zheng <jeff.zheng.6@gmail.com>
2015-12-05 20:49:18 -0800

    set up xCode project, adde added main views to storyboard

---
+++ Wim/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Wim
+//
+//  Created by Jeff Zheng on 12/5/15.
+//  Copyright © 2015 Jeff Zheng. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
